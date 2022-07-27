416697f9b7914de1fca7ce3dccf4ad41b2ce3667
Jacky-sansan <98638190+Jacky-sansan@users.noreply.github.com>
2022-07-27 23:44:23 +0800

    Add files via upload

---
+++ test/altHold_downward.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+"""
+move downward in altitude hold mode
+"""
+
+import sys
+# Import mavutil
+from pymavlink import mavutil
+import time
+
+# Create the connection
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+# Wait a heartbeat before sending commands
+master.wait_heartbeat()
+
+# Choose a mode
+mode = 'ALT_HOLD'
+
+# Check if mode is available
+if mode not in master.mode_mapping():
+    print('Unknown mode : {}'.format(mode))
+    print('Try:', list(master.mode_mapping().keys()))
+    sys.exit(1)
+
+# Get mode ID
+mode_id = master.mode_mapping()[mode]
+# Set new mode
+# master.mav.command_long_send(
+#    master.target_system, master.target_component,
+#    mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+#    0, mode_id, 0, 0, 0, 0, 0) or:
+# master.set_mode(mode_id) or:
+master.mav.set_mode_send(
+    master.target_system,
+    mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+    mode_id)
+
+# Arm
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
+# down
+for i in range(10):
+    master.mav.manual_control_send(
+        master.target_system,
+        0,   
+        0,
+        0,   # 0 to 1000, static=500, down<500, up>500
+        0,
+        0)
+
+# Disarm
+# master.arducopter_disarm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    0, 0, 0, 0, 0, 0, 0)
+
+# wait until disarming confirmed
+master.motors_disarmed_wait()
